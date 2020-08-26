d095d6623dcdd3c68b8fc63ab3651611a491a5e5
wottrich <wottrich78@gmail.com>
2020-08-25 22:56:55 -0300

    Init project

---
+++ app/src/main/java/wottrich/github/io/androidworkshop_crud/MainActivity.kt
@@ -0,0 +1,11 @@
+package wottrich.github.io.androidworkshop_crud
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
